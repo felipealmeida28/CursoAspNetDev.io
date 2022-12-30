6c35b1fd4c67166f3b49b8728423d709fb1e4e66
José Felipe Almeida Soares <jose.asoares@valid.com>
2022-12-30 15:45:01 -0300

    initial commit

---
+++ src/DevIO.App/Data/Migrations/20221230182830_initial identity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevIO.App.Data.Migrations
+{
+    public partial class initialidentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/DevIO.Data/Migrations/20221230182854_initial meu db.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevIO.Data.Migrations
+{
+    public partial class initialmeudb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Complemento",
+                table: "Enderecos",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Complemento",
+                table: "Enderecos",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+        }
+    }
+}
